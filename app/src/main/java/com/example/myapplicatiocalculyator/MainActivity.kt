f05c5ffdb0b3744e4b4576f10370489242219a68
ASELDOMasd <endenderbro@yandex.ru>
2026-07-04 23:58:16 +0300

    Чёт не получается, но тем неменее я продолжаю создавть приложение, чёт я намудрил с языками и адаптерами

--- app/src/main/java/com/example/myapplicatiocalculyator/MainActivity.kt
+++ app/src/main/java/com/example/myapplicatiocalculyator/MainActivity.kt
@@ -59,8 +59,31 @@ class MainActivity : AppCompatActivity() {
         val sectionText = findViewById<TextView>(R.id.sectionText)
 
 
+        setContentView(R.layout.activity_main)
 
-        supportActionBar?.title = ""
+        val spinner: Spinner = findViewById(R.id.language_spinner)
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.languages_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+
+            spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                    val selectedLang = parent.getItemAtPosition(position).toString()
+                    Toast.makeText(this@MainActivity, "Выбрано: $selectedLang", Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // Вызывается, если выбор пуст
+                }
+            }
+
+
+            supportActionBar?.title = ""
 
         soundPool = SoundPool.Builder().setMaxStreams(12).build()
 
@@ -103,6 +126,7 @@ class MainActivity : AppCompatActivity() {
             sectionText.visibility = View.GONE
             mainLayout.visibility = View.VISIBLE
         }
+
 
         // 🔊 кнопки 1–9
         val digitButtons = listOf(
@@ -157,12 +181,15 @@ class MainActivity : AppCompatActivity() {
                     else -> sectionText.text = ""
                 }
 
+
                 sectionText.visibility = View.VISIBLE
                 mainLayout.visibility = View.GONE
 
                 true
             }
     }
+
+
 
     override fun onDestroy() {
         super.onDestroy()

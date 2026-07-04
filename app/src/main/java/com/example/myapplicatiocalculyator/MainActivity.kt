1b6bed0049a5d1e8a74056413a24feb4b824fc9e
ASELDOMasd <endenderbro@yandex.ru>
2026-07-04 23:46:49 +0300

    Импортировал библеотеки для языков

--- app/src/main/java/com/example/myapplicatiocalculyator/MainActivity.kt
+++ app/src/main/java/com/example/myapplicatiocalculyator/MainActivity.kt
@@ -13,8 +13,6 @@ import com.google.android.material.navigation.NavigationView
 import android.widget.Button
 import android.content.Intent
 import android.widget.EditText
-import android.os.Bundle
-import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner

a2ba78d423759187e1aff6ea5317134094038677
ASELDOMasd <endenderbro@yandex.ru>
2026-09-02 23:41:25 +0300

    Удалил код с галочкой полностью

--- app/src/main/java/com/example/myapplicatiocalculyator/SettingsActivity.kt
+++ app/src/main/java/com/example/myapplicatiocalculyator/SettingsActivity.kt
@@ -7,6 +7,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.checkbox.MaterialCheckBox
 
+
 class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {

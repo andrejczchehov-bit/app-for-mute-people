adbd0800bbceb5993a32c0bd573942fc78f276f0
ASELDOMasd <endenderbro@yandex.ru>
2026-09-02 23:44:09 +0300

    Убрал лишнее из activity_settings.xml

--- app/src/main/java/com/example/myapplicatiocalculyator/SettingsActivity.kt
+++ app/src/main/java/com/example/myapplicatiocalculyator/SettingsActivity.kt
@@ -7,7 +7,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.checkbox.MaterialCheckBox
 
-
+var
 class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {

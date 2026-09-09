c9ee22cbf56c8fb3dcd2b7546393823c15de436a
ASELDOMasd <endenderbro@yandex.ru>
2026-09-10 02:15:08 +0300

    Удалил изменения с галочкой

--- app/src/main/java/com/example/myapplicatiocalculyator/SettingsActivity.kt
+++ app/src/main/java/com/example/myapplicatiocalculyator/SettingsActivity.kt
@@ -6,9 +6,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.checkbox.MaterialCheckBox
-import com.example.myapplicatiocalculyator.databinding.ActivityCheckBoxBinding
 
-var checkBoxBinding:ActivityCheckBoxBinding
+
+
 class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
